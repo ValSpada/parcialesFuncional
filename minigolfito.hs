d655619e6c6aacd7647844035e491cd35f15158d
ValSpada <vspada@frba.utn.edu.ar>
2024-06-10 19:22:58 -0300

    Corregido hasta punto 3, faltan punto 4 y 5

--- minigolfito.hs
+++ minigolfito.hs
@@ -36,72 +36,110 @@ mayorSegun f a b
 
 ------------------ PUNTO 1 ------------------
 
-type PaloGolf = Jugador -> Tiro
+type Palo = Habilidad -> Tiro
+--type PaloGolf = Jugador -> Tiro
 
 -- a.i.   --
-putter :: PaloGolf
-putter (Jugador _ _ unaHabilidad) = Tiro 10 precisionRecibida 0
-    where precisionRecibida = (* 2) . precisionJugador $ unaHabilidad
+putter :: Palo
+putter unaHabilidad = Tiro 10 precisionRecibida 0
+    where precisionResultante = (* 2) . precisionJugador $ unaHabilidad
 
 -- a.ii.  --
-madera :: PaloGolf
-madera (Jugador _ _ unaHabilidad) = Tiro 100 precisionRecibida 5
-    where precisionRecibida = flip div 2 . precisionJugador $ unaHabilidad
+madera :: Palo
+madera unaHabilidad = Tiro 100 precisionResultante 5
+    where precisionResultante = flip div 2 . precisionJugador $ unaHabilidad
 
 -- a.iii. --
-hierro :: Int -> PaloGolf
-hierro n (Jugador _ _ unaHabilidad) = Tiro velocidadResultante precisionRecibida alturaResultante
+hierro :: Int -> Palo
+hierro n unaHabilidad = Tiro velocidadResultante precisionResultante alturaResultante
     where 
     velocidadResultante = fuerzaJugador unaHabilidad * n
-    precisionRecibida   = flip div n . precisionJugador $ unaHabilidad
+    precisionResultante = flip div n . precisionJugador $ unaHabilidad
     alturaResultante    = max 0 . (subtract 3) $ n 
 
 -- b. --
-palos :: [PaloGolf]
-palos = [putter, madera, hierro 1, hierro 2, hierro 3, hierro 4, hierro 5, hierro 6, hierro 7, hierro 8, hierro 9, hierro 10]
+palos :: [Palo]
+palos = [putter, madera] ++ map hierro [1..10]
+--palos = [putter, madera, hierro 1, hierro 2, hierro 3, hierro 4, hierro 5, hierro 6, hierro 7, hierro 8, hierro 9, hierro 10] 
 
 ------------------ PUNTO 2 ------------------
 
-golpe :: PaloGolf -> Jugador -> Tiro
-golpe unPalo = unPalo
+golpe :: Palo -> Jugador -> Tiro
+golpe unPalo = unPalo . habilidad
+--golpe unPalo = unPalo
 
 ------------------ PUNTO 3 ------------------
 
-type Obstaculo = Tiro -> Tiro
+--type Obstaculo = Tiro -> Tiro
+data Obstaculo = Obstaculo {
+  criterioSuperacion :: Tiro -> Bool,
+  efectoSobreTiro    :: Tiro -> Tiro
+}
+
+tirarEnObstaculo :: Obstaculo -> Tiro -> Tiro
+tirarEnObstaculo unObstaculo unTiro
+  | criterioSuperacion unObstaculo unTiro = efectoSobreTiro unObstaculo unTiro
+  | otherwise                             = Tiro 0 0 0
 
 -- a. --
 tunelConRampita :: Obstaculo
-tunelConRampita unTiro
-    | superaTunel unTiro = Tiro (2 * velocidad unTiro) 100 0
-    | otherwise          = Tiro 0 0 0
+tunelConRampita = Obstaculo{
+    criterioSuperacion = superaTunel,
+    efectoSobreTiro    = pasarPorTunel
+}
+
+superaTunel :: Tiro -> Bool
+superaTunel (Tiro _ unaPrecision unaAltura) = unaPrecision > 90 && unaAltura == 0
+
+pasarPorTunel :: Tiro -> Tiro
+pasarPorTunel unTiro = Tiro (2 * velocidad unTiro) 100 0
+
+--tunelConRampita :: Obstaculo
+--tunelConRampita unTiro
+--    | superaTunel unTiro = Tiro (2 * velocidad unTiro) 100 0
+--    | otherwise          = Tiro 0 0 0
 
 -- b. --
 laguna :: Int -> Obstaculo
-laguna largo unTiro
-    | superaLaguna unTiro = Tiro (velocidad unTiro) (precision unTiro) (div (altura unTiro) largo)
-    | otherwise           = Tiro 0 0 0
-
--- c. --
-hoyo :: Obstaculo
-hoyo unTiro
-    | superaHoyo unTiro = Tiro 0 0 0
-    | otherwise         = Tiro 0 0 0
-
--- Auxiliar
-superaTunel :: Tiro -> Bool
-superaTunel (Tiro _ unaPrecision unaAltura) = unaPrecision > 90 && unaAltura == 0
+laguna largo = Obstaculo{
+    criterioSuperacion = superaLaguna,
+    efectoSobreTiro    = pasarPorLaguna largo
+}
 
 superaLaguna :: Tiro -> Bool
 superaLaguna (Tiro unaVelocidad _ unaAltura) = unaVelocidad > 80 && between 1 5 unaAltura 
 
+pasarPorLaguna :: Int -> Tiro -> Tiro
+pasarPorLaguna largo unTiro = Tiro (velocidad unTiro) (precision unTiro) (div (altura unTiro) largo)
+
+--laguna :: Int -> Obstaculo
+--laguna largo unTiro
+--    | superaLaguna unTiro = Tiro (velocidad unTiro) (precision unTiro) (div (altura unTiro) largo)
+--    | otherwise           = Tiro 0 0 0
+
+-- c. --
+laguna :: Int -> Obstaculo
+laguna largo = Obstaculo{
+    criterioSuperacion = superaHoyo,
+    efectoSobreTiro    = pasarPorHoyo
+}
+
 superaHoyo :: Tiro -> Bool
 superaHoyo (Tiro unaVelocidad unaPrecision unaAltura) = between 5 20 unaVelocidad && unaPrecision > 95 && unaAltura == 0
+
+pasarPorHoyo :: Tiro -> Tiro
+pasarPorHoyo _ = Tiro 0 0 0
+
+--hoyo :: Obstaculo
+--hoyo unTiro
+--    | superaHoyo unTiro = Tiro 0 0 0
+--    | otherwise         = Tiro 0 0 0
 
 ------------------ PUNTO 4 ------------------
 
 -- a. --
 
-palosUtiles :: Jugador -> Obstaculo -> [PaloGolf]
+palosUtiles :: Jugador -> Obstaculo -> [Palo]
 palosUtiles unJugador unObstaculo = filter (flip superaObstaculo unObstaculo . flip golpe unJugador) palos
 
 -- b. --
